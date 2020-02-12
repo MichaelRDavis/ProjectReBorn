fa2e94a9b53e35a6fb195f402e4b6ed9e287fd8b
MichaelRDavis <MichaelRDavis1991@protonmail.com>
2020-02-12 22:00:10 +0000

    Added AI base code

---
+++ Source/DMC_GameProject/Public/AI/ReBorn_Zombie.h
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Player/ReBorn_BaseCharacter.h"
+#include "ReBorn_Zombie.generated.h"
+
+UCLASS()
+class DMC_GAMEPROJECT_API AReBorn_Zombie : public AReBorn_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/DMC_GameProject/Public/AI/ReBorn_ZombieAI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "ReBorn_ZombieAI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DMC_GAMEPROJECT_API AReBorn_ZombieAI : public AAIController
+{
+	GENERATED_BODY()
+	
+};
